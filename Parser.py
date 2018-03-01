78415a3732d1e9bff55e35aa5c0fadbe9d88fc1e
jonahar <jonahar@gmail.com>
2018-03-01 20:44:18 +0200

    parser

---
+++ Parser.py
@@ -0,0 +1,28 @@
+from tokenizer import Tokenizer
+from Ride import Ride
+from Car import Car
+
+
+def build_data(input_file):
+    tokenizer = Tokenizer(input_file)
+    rows = tokenizer.next_int()
+    cols = tokenizer.next_int()
+    num_cars = tokenizer.next_int()
+    num_rides = tokenizer.next_int()
+    bonus = tokenizer.next_int()
+    total_steps = tokenizer.next_int()
+    tokenizer.next_line()
+    # building all rides
+    rides = []
+    for i in range(num_rides):
+        a = tokenizer.next_int()
+        b = tokenizer.next_int()
+        x = tokenizer.next_int()
+        y = tokenizer.next_int()
+        s = tokenizer.next_int()
+        f = tokenizer.next_int()
+        rides.append(Ride(a, b, x, y, s, f))
+    cars = []
+    for i in range(num_cars):
+        cars.append(Car())
+    return rides, cars
--- tokenizer.py
+++ tokenizer.py
@@ -1,11 +1,10 @@
-#Road to Dublin
+# Road to Dublin
 
 class Tokenizer:
     def __init__(self, input_file):
         self.file = open(input_file, 'r')
         self.cur_line = self.file.readline()
-        self.eof  = False
-
+        self.eof = False
 
     def next_word(self):
         """
@@ -16,20 +15,17 @@ class Tokenizer:
         :return: the next integer in the line.
         """
         self.cur_line = self.cur_line.lstrip()
-        if not self.cur_line: #end of line
+        if not self.cur_line:  # end of line
             self.next_line()
             return None
         cur_split = self.cur_line.split(maxsplit=1)
-        if (len(cur_split) == 1): #end of line
+        if (len(cur_split) == 1):  # end of line
             self.cur_line = ""
-        else: #merging second cell to string
+        else:  # merging second cell to string
             self.cur_line = cur_split[1]
 
-        #returning current int:
+        # returning current int:
         return cur_split[0]
-
-
-
 
     def next_int(self):
         """
@@ -43,13 +39,13 @@ class Tokenizer:
 
         :return: the next char in the line we're reading, None if end of line.
         """
-        #searching for the first char which isn't a whitespace:
+        # searching for the first char which isn't a whitespace:
 
         self.cur_line = self.cur_line.lstrip()
-        if not self.cur_line: #end of line
+        if not self.cur_line:  # end of line
             self.next_line()
             return None
-        else: #reading char
+        else:  # reading char
             c = self.cur_line[0]
             if (len(self.cur_line) > 1):
                 self.cur_line = self.cur_line[1:]
@@ -57,22 +53,16 @@ class Tokenizer:
                 self.cur_line = ""
             return c
 
-
-
-
     def next_line(self):
         """
         reads next line into cur_line and updates other variables.
         :return:
         """
-        line  = self.cur_line
+        line = self.cur_line
         self.cur_line = self.file.readline()
         if not self.cur_line:
             self.eof = True
         return line
-
-
-
 
     def is_eof(self):
         """
@@ -80,5 +70,3 @@ class Tokenizer:
         :return: whether we've reached the end of the file
         """
         return self.eof
-
-
