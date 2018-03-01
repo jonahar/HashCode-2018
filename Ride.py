73d2a935ac6a90de3eefa4c5156590ebcb8955ef
GalPatel <04011993gal!@#>
2018-03-01 20:39:15 +0200

    basic Ride

---
+++ Ride.py
@@ -0,0 +1,23 @@
+class Ride:
+    def __init__(self, a, b, x, y, s, f):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        self.s = s
+        self.f = f
+
+    def start_point(self):
+        return (self.a, self.b)
+
+    def end_point(self):
+        return (self.x, self.y)
+
+    def start_time(self):
+        return self.s
+
+    def end_time(self):
+        return self.e
+
+    def total_length(self):
+        return
